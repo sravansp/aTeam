188e44af9b0214fb2e798236bdaed0cc6ba374f9
Your Name <your.email@example.com>
2024-02-10 03:06:41 -0800

    Added new Page for serach other changes in dashboard

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 const body = document.querySelector("body"),
   sidebar = body.querySelector(".sidebar"),
+  nav = body.querySelector(".navbar"),
   sidebarToggle = body.querySelector(".app-sidebar__toggle"),
   toggle = body.querySelector(".hamburger"),
   searchBtn = body.querySelector(".search-box");
@@ -70,6 +71,96 @@ $(sidebarToggle).click(function (e) {
     $(sidebar).addClass("open");
   }
 });
+
+
+
+const navHeight = 70;
+// the point the scroll starts from (in px)
+let lastScrollY = 0;
+// how far to scroll (in px) before triggering
+const delta = 10;
+// function to run on scrolling
+function scrolled() {
+  let sy = window.scrollY;
+  // only trigger if scrolled more than delta
+  if (Math.abs(lastScrollY - sy) > delta) {
+    // scroll down -> hide nav bar
+    if (sy > lastScrollY && sy > navHeight) {
+      nav.classList.add("nav-up");
+    } 
+    // scroll up -> show nav bar
+    else if (sy < lastScrollY) {
+      nav.classList.remove("nav-up");
+      // Remove the "nav_fixed" class when reaching the top of the page
+      if (sy === 0) {
+        nav.classList.remove("nav_fixed");
+      } else {
+        nav.classList.add("nav_fixed");
+      }
+    }
+    // update current scroll point
+    lastScrollY = sy;
+  }
+}
+
+
+// Add event listener & debounce so not constantly checking for scroll
+let didScroll = false;
+window.addEventListener("scroll", function(e){
+  didScroll = true;
+});
+
+setInterval(function() {
+  if (didScroll) {
+    scrolled();
+    didScroll = false;
+   }
+}, 250)
+
+
+
+
+
+
+// Function to initialize dropdown functionality
+function initializeDropdown(optionMenu) {
+  const selectBtn = optionMenu.querySelector(".select-btn"),
+    options = optionMenu.querySelectorAll(".option"),
+    sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+  selectBtn.addEventListener("click", () => {
+    // Close all other dropdowns before opening the clicked one
+    document.querySelectorAll(".select-menu").forEach((menu) => {
+      if (menu !== optionMenu) {
+        menu.classList.remove("active");
+      }
+    });
+
+    optionMenu.classList.toggle("active");
+  });
+
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      let selectedOption = option.querySelector(".option-text").innerText;
+      sBtn_text.innerText = selectedOption;
+
+      // Close the dropdown after selecting an option
+      optionMenu.classList.remove("active");
+    });
+  });
+}
+
+// Get all dropdown menus with the class name "select-menu"
+const allDropdowns = document.querySelectorAll(".select-menu");
+
+// Initialize each dropdown menu
+allDropdowns.forEach((dropdown) => {
+  initializeDropdown(dropdown);
+});
+
+
+
+
 
 /*===== SCROLL REVEAL ANIMATION =====*/
 function ScrollRevealaAm() {
